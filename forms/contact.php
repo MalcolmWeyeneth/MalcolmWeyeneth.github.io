53de1efb5a17e06e4caa1462b891c1b3d887ed79
Malcolm Weyeneth <76868178+MalcolmWeyeneth@users.noreply.github.com>
2021-04-22 10:23:00 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
   // Receiving email address
-  $receiving_email_address = 'malcolm.weyeneth@outlook.com';
+  $receiving_email_address = malcolm.weyeneth@outlook.com;
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
